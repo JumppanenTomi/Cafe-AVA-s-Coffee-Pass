3b5984592652b56d3eae6aa863490b7297824537
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-02-29 17:47:36 +0200

    Add QR code generation component
    A new component, QrCodeGen, has been introduced. This component uses the 'qrcode' package to generate a QR code, given input text, error correction level, and width. Consequently, the qrcode package and its types (@types/qrcode) have been added as dependencies to the project.

---
+++ components/QrCodeGen.tsx
@@ -0,0 +1,43 @@
+// Created by Tomi Jumppanen on 29.02.2024
+"use client"
+import {useEffect, useRef, useState} from "react";
+import qrcode, {QRCodeErrorCorrectionLevel} from "qrcode";
+
+interface QrCodeGenProps {
+	text: string,
+	errorCorrectionLevel?: QRCodeErrorCorrectionLevel,
+	width?: number
+}
+
+const QrCodeGen = ({text, errorCorrectionLevel = "high", width = 500}: QrCodeGenProps) => {
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+	const [genError, setGenError] = useState<string | undefined>()
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+
+		if (canvas) {
+			qrcode.toCanvas(
+				canvas,
+				text,
+				{errorCorrectionLevel, width},
+				err => {
+					if (err) {
+						setGenError(err.toString())
+					}
+				}
+			)
+		}
+	}, [text, errorCorrectionLevel, width]);
+
+	return !genError ? (
+		<canvas ref={canvasRef}/>
+	) : (
+		<div>
+			<h2>There was an error creating QR code:</h2>
+			<p>{genError}</p>
+		</div>
+	)
+};
+
+export default QrCodeGen;
