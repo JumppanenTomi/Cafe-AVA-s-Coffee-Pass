e0e5b1e316ffe336e0715478d9f12ca36c19f91d
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-03-11 22:15:13 +0200

    Update navigation component and theme colors
    The commit introduces a logo in the navigation bar and updates the design by changing the class properties of the Navigation component. Additionally, the color scheme in app/globals.css and tailwind.config.js files is refactored, simplifying and redefining the variable values for background and foreground colors.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,16 @@
 // Created by Tomi Jumppanen on 29.02.2024
 
 import AuthButton from "@/components/AuthButton";
+import Image from "next/image";
+import logo from "@/public/logo.png"
 
 const Nav = () => {
 	return (
-		<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-			<div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
+		<nav className="w-full flex justify-between h-16 bg-background">
+			<div className="justify-center items-center p-3 text-sm">
+				<Image src={logo} alt={"Cafe AVA- Logo"} width={50} height={40}/>
+			</div>
+			<div className="justify-center items-center p-3 text-sm">
 				<AuthButton/>
 			</div>
 		</nav>
