a75a47885bcf6c549fe1bffd4451609a8e648d16
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-03-12 19:29:00 +0200

    Refactor authentication process and improve user interface
    This update restructures the Login and Register components, enhancing usability by replacing specific elements with reusable components such as Form, EmailInput, and FormSubmitButton. Additionally, it introduces an image rendering component for the website logo. Changes in global.css improve the visibility and the user interface. The authentication process has been also updated to pass message and isError parameters through a redirect enhancing error handling and feedback to the user.

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 import {headers} from "next/headers";
 import {createClient} from "@/utils/supabase/server";
 import {redirect} from "next/navigation";
-import {SubmitButton} from "./submit-button";
+import Image from "next/image";
+import logo from "@/public/logo.png";
+import {Form} from "@/components/Inputs/Form";
+import EmailInput from "@/components/Inputs/EmailInput";
+import {FormSubmitButton} from "@/components/Inputs/FormSubmitButton";
 
-export default function Register({searchParams,}: { searchParams: { message: string }; }) {
+export default function Register({searchParams}: { searchParams: { isError: string, message: string }; }) {
 	const signUp = async (formData: FormData) => {
 		"use server";
 
@@ -21,59 +25,35 @@ export default function Register({searchParams,}: { searchParams: { message: str
 
 		if (error) {
 			console.log("error", error)
-			return redirect("/auth/register?message=Could not authenticate user");
+			return redirect("/auth/register?isError=true&message=Could not authenticate user");
 		}
 
-		return redirect("/auth/register?message=Check email to continue sign in process");
+		return redirect("/auth/register?isError=false&message=Check email to continue sign in process");
 	};
 
 	return (
-		<div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-			<Link
-				href="/"
-				className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="2"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-				>
-					<polyline points="15 18 9 12 15 6"/>
-				</svg>
-				{" "}
-				Back
-			</Link>
-
-			<form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-				<label className="text-md" htmlFor="email">
-					Email
-				</label>
-				<input
-					className="rounded-md px-4 py-2 bg-inherit border mb-6"
-					name="email"
-					placeholder="you@example.com"
-					required
-				/>
-				<SubmitButton
-					formAction={signUp}
-					className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-					pendingText="Signing Up..."
-				>
-					Sign Up
-				</SubmitButton>
-				{searchParams?.message && (
-					<p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-						{searchParams.message}
-					</p>
-				)}
-			</form>
+		<div className="flex-1 w-full flex flex-col items-center">
+			<div
+				className="flex flex-col items-center justify-center gap-5 p-5 w-full flex-grow bg-[url('/coffee.jpg')] bg-cover bg-top">
+				<Image src={logo} alt={"ava logo"} width={100}/>
+			</div>
+			<div className="flex flex-col items-center justify-center gap-5 py-16 w-full max-w-screen-sm p-5">
+				<h1 className={'font-bold text-3xl'}>Register</h1>
+				<p className={'text-center font-medium max-w-screen-sm'}>Once you have submitted your email, a sign-in
+					link will be sent directly to your inbox. Using this link, you can securely access website.</p>
+				<Form isError={searchParams.isError == "true"} error={searchParams.message}>
+					<EmailInput/>
+					<div className={"flex items-center w-full gap-4"}>
+						<Link href={"/"} className={'btn-secondary w-full'}>Back</Link>
+						<FormSubmitButton
+							formAction={signUp}
+							pendingText="Signing In..."
+						>
+							Sign Up
+						</FormSubmitButton>
+					</div>
+				</Form>
+			</div>
 		</div>
 	);
 }
--- components/Inputs/FormSubmitButton.tsx
+++ components/Inputs/FormSubmitButton.tsx
@@ -13,7 +13,7 @@ export function FormSubmitButton({children, pendingText, ...props}: Props) {
 	const isPending = pending && action === props.formAction;
 
 	return (
-		<button {...props} type="submit" aria-disabled={pending}>
+		<button {...props} className={'btn-primary w-full'} type="submit" aria-disabled={pending}>
 			{isPending ? pendingText : children}
 		</button>
 	);
