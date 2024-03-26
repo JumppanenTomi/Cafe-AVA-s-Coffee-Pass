fd3ce830d40ac112266cb427a706f53797dd8e70
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-03-26 02:43:34 +0200

    Implement stats counting in Statistics component
    The 'Statistics' component has been updated to count statistics using a new function, 'getAllStaps'. This function fetches the logged in user's stamp counts from the 'stamp_logs' database table using the supabase client. The displayed 'Total Stamps' now accurately reflects the current user's count.

--- components/Statistics.tsx
+++ components/Statistics.tsx
@@ -1,14 +1,30 @@
-export function Statistics(){
-	//TODO: Add logic to count stats
+import {createClient} from "@/utils/supabase/server";
+
+const getAllStaps = async () => {
+	'use server'
+	const supabase = createClient()
+	const user = await supabase.auth.getUser()
+	const userId = user.data.user?.id
+	const {count, error} = await supabase
+		.from("stamp_logs")
+		.select('*', {count: 'exact', head: true}) // exact, planned, or executed
+		.eq("user_id", userId)
+
+	if (error) console.log(error)
+	return count
+}
+
+export async function Statistics() {
+	const stamps = await getAllStaps()
 	return (
 		<div className={'white-container w-full flex flex-wrap justify-between text-center'}>
 			<div>
-				<h4><b>Total Visits</b></h4>
+				<h4><b>Days Visited</b></h4>
 				<h4>--</h4>
 			</div>
 			<div>
 				<h4><b>Total Stamps</b></h4>
-				<h4>--</h4>
+				<h4>{stamps}</h4>
 			</div>
 		</div>
 	)
