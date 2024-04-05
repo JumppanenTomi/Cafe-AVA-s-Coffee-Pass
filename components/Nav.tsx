3a34fc1ea268a267251f3095ecc5425fc2897aba
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-04-06 01:49:00 +0300

    Refactor user role retrieval and improve navigation
    This commit introduces the `getRole` utility function for more efficient user role retrieval and simplifies related logic in `app/page.tsx` and `components/Nav.tsx`. Additionally, an admin dashboard link is now conditionally displayed in the navigation bar for users with 'owner' or 'barista' roles.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,18 +5,22 @@ import logo from "@/public/logo.png"
 import {faUser} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import getRole from "@/utils/getRole";
 
 interface NavProps {
 	title?: string,
 }
 
-const Nav = ({title}: NavProps) => {
+export default async function Nav ({title}: NavProps) {
+	const userRole = await getRole()
+
 	return (
 		<nav className="w-full flex justify-between items-center h-16 p-5 bg-background">
 			<Link href={"/client"}>
 				<Image src={logo} alt={"Cafe AVA- Logo"} width={50}/>
 			</Link>
 			<div className={'w-full flex justify-end items-center gap-4'}>
+				{(userRole === 'owner' || userRole === 'barista') && <Link href={'/admin'} className={'btn-primary'}>Admin Dashboard</Link>}
 				<Link href={"/client/settings"}>
 					<FontAwesomeIcon icon={faUser} size={"xl"}/>
 				</Link>
@@ -25,4 +29,3 @@ const Nav = ({title}: NavProps) => {
 	)
 }
 
-export default Nav
