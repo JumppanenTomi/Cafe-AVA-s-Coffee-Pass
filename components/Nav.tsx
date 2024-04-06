6fe12128cfe294b838a6ab8cfc3ebee390a9d48c
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-04-06 18:01:26 +0300

    Add leaflet map and FAQ page
    This update includes the implementation of a new Frequently Asked Questions (FAQ) page which includes a dynamically imported leaflet map. Also added a heroicons dependency and created a new Map component. Updated the Nav component to include a link to the FAQ page and updated both react and react-dom versions. The Map component offers a Leaflet map which is compatible with Webpack's CSS import.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,6 +5,7 @@ import logo from "@/public/logo.png"
 import {faUser} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import {QuestionMarkCircleIcon} from "@heroicons/react/20/solid";
 
 interface NavProps {
 	title?: string,
@@ -13,10 +14,13 @@ interface NavProps {
 const Nav = ({title}: NavProps) => {
 	return (
 		<nav className="w-full flex justify-between items-center h-16 p-5 bg-background">
+			<Link href={'/client/faq'}>
+				<QuestionMarkCircleIcon className={'h-9 w-9'}/>
+			</Link>
 			<Link href={"/client"}>
 				<Image src={logo} alt={"Cafe AVA- Logo"} width={50}/>
 			</Link>
-			<div className={'w-full flex justify-end items-center gap-4'}>
+			<div className={'flex justify-end items-center gap-4'}>
 				<Link href={"/client/settings"}>
 					<FontAwesomeIcon icon={faUser} size={"xl"}/>
 				</Link>
