a1901b17b5ea3ed6121161a0e535ee577900aebc
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-04-08 22:55:17 +0300

    If user is logged in redirect to client page. Also removed conditional redirection, because now we have button to toggle between admin and client views

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 import Image from "next/image";
-import logo from "@/public/logo.png"
+import logo from "@/public/logo.png";
 import AuthButton from "@/components/AuthButton";
-import {redirect} from "next/navigation";
-import getRole from "@/utils/getRole";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
-	const userRole = await getRole()
-	if (userRole === 'client' || userRole === null) {
-		return redirect('/client')
-	}
-	if (userRole === 'owner' || userRole === 'barista') {
-		return redirect('/admin')
-	}
+	const supabase = createClient();
+	const {
+		data: {user},
+	} = await supabase.auth.getUser();
 
+	if (user) {
+		return redirect("/client");
+	}
 
 	return (
-		<div className="flex-1 w-full flex flex-col items-center">
+		<div className="flex flex-col items-center flex-1 w-full">
 			<div className="flex flex-col items-center justify-center gap-5 p-5 w-full flex-grow bg-[url('/coffee.jpg')] bg-cover bg-top">
 				<Image src={logo} alt={"ava logo"} width={100} />
 			</div>
-			<div className="flex flex-col items-center justify-center gap-5 py-16 w-full max-w-screen-sm p-5">
+			<div className="flex flex-col items-center justify-center w-full max-w-screen-sm gap-5 p-5 py-16">
 				<h1 className={'font-bold text-3xl'}>Welcome!</h1>
 				<p className={'text-center font-medium max-w-screen-sm'}>Welcome to our Digital Coffee Pass! Start your digital coffee journey with us today!</p>
 				<AuthButton />
