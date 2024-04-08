54e6a3bd3d1e1fd07493781a1dad462bf50fc541
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-04-08 22:50:54 +0300

    Move authentication server actions to separate file and wrap them with try catch for better error handling

---
+++ utils/ServerActions/authentication.ts
@@ -0,0 +1,86 @@
+"use server";
+import { redirect } from "next/navigation";
+import { createClient } from "../supabase/server";
+
+export const signOut = async () => {
+  const supabase = createClient();
+  await supabase.auth.signOut();
+  return redirect("/");
+};
+
+export const signIn = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const supabase = createClient();
+
+  try {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        shouldCreateUser: false,
+        emailRedirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL!}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return redirect(
+      "/auth/login?isError=false&message=Check email to continue sign in process"
+    );
+  } catch (error) {
+    //TODO: Add logging
+    return redirect(
+      "/auth/login?isError=true&message=Could not authenticate user"
+    );
+  }
+};
+
+export const signUp = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const supabase = createClient();
+
+  try {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL!}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return redirect(
+      "/auth/register?isError=false&message=Check email to continue sign in process"
+    );
+  } catch (error) {
+    //TODO: Add logging
+    return redirect(
+      "/auth/register?isError=true&message=Could not authenticate user"
+    );
+  }
+};
+
+export const changeEmail = async (formData: FormData) => {
+  const supabase = createClient();
+  const newEmail = formData.get("email") as string;
+
+  try {
+    const { data, error } = await supabase.auth.updateUser({
+      email: newEmail,
+    });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return redirect(
+      "/auth/updateEmail?message=Check email for confirmation&isError=false"
+    );
+  } catch (error: any) {
+    //TODO: Add logging
+    throw new Error(`Failed to update email: ${error.message}`);
+  }
+};
