274ba8b18d5dc65e167146c016040d8173356810
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-04-09 16:37:17 +0300

    should work now

--- utils/ServerActions/authentication.ts
+++ utils/ServerActions/authentication.ts
@@ -58,8 +58,6 @@ export const authenticateWithGoodle = async () => {
         redirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL!}/auth/callback`,
       },
     });
-    console.log(`${process.env.NEXT_PUBLIC_VERCEL_URL!}/auth/callback`)
-    console.log(data)
     if (error) { throw new Error(error.message) }
     data && (url = data.url)
   } catch (error: any) {
