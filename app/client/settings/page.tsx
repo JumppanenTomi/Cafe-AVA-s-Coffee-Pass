f822e4b3be889889442f5014c312360d31ab74ed
JumppanenTomi <tomi.jumppanen@hotmail.com>
2024-04-25 19:57:03 +0300

    Fixed styling issues and refactored to follow same styling

--- app/client/settings/page.tsx
+++ app/client/settings/page.tsx
@@ -3,6 +3,7 @@ import AuthButton from "@/components/Inputs/buttons/AuthButton";
 import LinkButton from "@/components/Inputs/buttons/LinkButton";
 import GetDataButton from "@/components/Inputs/buttons/GetDataButton";
 import BackButton from "@/components/Inputs/buttons/BackButton";
+import FadeIn from "@/components/Animations/Render/FadeIn";
 
 export default async function SettingsPage() {
   const supabase = createClient();
@@ -12,7 +13,7 @@ export default async function SettingsPage() {
 
   return (
     <div className='flex flex-col justify-between flex-grow w-full'>
-      <div>
+      <FadeIn duration={0.8}>
         <h1 className='mb-10'>Settings</h1>
         <div className='flex flex-col items-start gap-5'>
           <LinkButton
@@ -22,7 +23,7 @@ export default async function SettingsPage() {
           <GetDataButton />
           <AuthButton />
         </div>
-      </div>
+      </FadeIn>
       <BackButton />
     </div>
   );
--- app/client/vouchers/page.tsx
+++ app/client/vouchers/page.tsx
@@ -1,29 +1,27 @@
 import { createClient } from "@/utils/supabase/server";
-import Nav from "@/components/navigation/Nav";
 import { Suspense } from "react";
 import BackButton from "@/components/Inputs/buttons/BackButton";
 import VoucherList from "@/components/lists/VoucherList";
-import {
-	fetchAllVouchers,
-} from "@/utils/ServerActions/voucher";
+import { fetchAllVouchers } from "@/utils/ServerActions/voucher";
 import FadeIn from "@/components/Animations/Render/FadeIn";
 
 export default async function VouchersPage() {
-	const initialVouchers = await fetchAllVouchers();
-	const supabase = createClient();
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
-	return (
-		<Suspense>
-			<Nav />
-			<FadeIn duration={1} className='w-full'>
-				<VoucherList
-					initialVouchers={initialVouchers}
-					userId={user?.id || ""}
-				/>
-			</FadeIn>
-			<BackButton />
-		</Suspense>
-	);
+  const initialVouchers = await fetchAllVouchers();
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return (
+    <div className='flex flex-col flex-grow gap-5'>
+      <Suspense fallback={"Loading..."}>
+        <FadeIn duration={1} className='flex-grow w-full '>
+          <VoucherList
+            initialVouchers={initialVouchers}
+            userId={user?.id || ""}
+          />
+        </FadeIn>
+      </Suspense>
+      <BackButton />
+    </div>
+  );
 }
