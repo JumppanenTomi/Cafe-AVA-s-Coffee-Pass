56f79ae804369505e3bbfb30b2932bd08d2af4ce
tuomas01 <heikkilatuom@gmail.com>
2024-05-05 12:36:48 +0300

    Fixed AutoCompleteInput and now it should work on IOS as well

--- components/Inputs/AutoCompleteInput.tsx
+++ components/Inputs/AutoCompleteInput.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
 
 interface AutoCompleteInputProps {
@@ -26,44 +27,72 @@ export default function AutoCompleteInput({
 }: AutoCompleteInputProps) {
   const [openDropdown, setOpenDropdown] = useState(false);
   const [inputValue, setInputValue] = useState(defaultValue || "");
+  const [selectedOption, setSelectedOption] = useState<string | undefined>();
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
-    console.log(value);
     setInputValue(value);
+    setSelectedOption("");
     onInputChange(value);
+    setOpenDropdown(true);
   };
 
-  const handleOptionClick = (value: string) => {
+  const handleOptionClick = (value: string, selectedOptionValue: string) => {
     setInputValue(value);
     onInputChange(value);
+    setSelectedOption(selectedOptionValue);
     setOpenDropdown(false);
   };
 
+  const reset = () => {
+    setInputValue("");
+    setSelectedOption("");
+    setOpenDropdown(true);
+  };
+
   return (
-    <div style={{ position: "relative" }}>
+    <div className='relative'>
       {showLabel && (
         <label className='input-label' htmlFor={inputName}>
           {inputLabel} {isRequired && <label>*</label>}
         </label>
       )}
-      <input
-        className='input'
-        name={inputName}
-        placeholder={inputPlaceholder}
-        required={isRequired}
-        value={inputValue}
-        onChange={handleInputChange}
-        onFocus={() => setOpenDropdown(true)}
-        onBlur={() => setOpenDropdown(false)}
-      />
-      <div className='dropdown'>
-        {options.map((opt, index) => (
-          <div key={index} onClick={() => handleOptionClick(opt.id.toString())}>
-            {opt.label}
-          </div>
-        ))}
+      <label className='text-xs input-label' htmlFor={inputName}>
+        {selectedOption}
+      </label>
+      <div className='flex items-center gap-2'>
+        <input
+          className='input'
+          name={inputName}
+          placeholder={inputPlaceholder}
+          required={isRequired}
+          value={inputValue}
+          onChange={handleInputChange}
+          onFocus={() => setOpenDropdown(true)}
+          onBlur={() => setTimeout(() => setOpenDropdown(false), 200)}
+        />
+        {inputValue && inputValue.toString().length > 0 && (
+          <XMarkIcon className='h-8 cursor-pointer' onClick={reset} />
+        )}
       </div>
+      {openDropdown && options.length > 0 && (
+        <ul
+          className={
+            "absolute bg-white z-40 w-full py-4 rounded-md border max-h-40 overflow-y-auto"
+          }
+        >
+          {options.map((opt, index) => (
+            <li
+              className={"hover:bg-black/10 px-4 py-3 cursor-pointer"}
+              style={{ textTransform: "none" }}
+              key={index}
+              onClick={() => handleOptionClick(opt.id.toString(), opt.label)}
+            >
+              {opt.label}
+            </li>
+          ))}
+        </ul>
+      )}
       {helperText && <p className='input-helper-text'>{helperText}</p>}
     </div>
   );
