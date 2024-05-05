65048450671b98d43ae24e0441095ac06b9b68b1
tuomas01 <heikkilatuom@gmail.com>
2024-05-05 20:06:08 +0300

    Fixed UI styling for redeemVoucher page

--- app/admin/redeemVoucher/[...slug]/page.tsx
+++ app/admin/redeemVoucher/[...slug]/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useMemo, useState } from "react";
 import { fetchUsers } from "@/utils/ServerActions/user";
 import { fetchAllVouchers } from "@/utils/ServerActions/voucher";
 import RedeemVoucher from "../../vouchers/redeemVoucher";
+import formatDateToFinnish from "@/utils/formatDateToFinnish";
 
 /**
  * Page where owner/barista gets redirected to when scanning a voucher's QR code
@@ -40,17 +41,20 @@ export default function Page({ params }: { params: { slug: string[] } }) {
 
   return user && voucher ? (
     <div className='flex flex-col items-center justify-center flex-1 w-full gap-5 p-5'>
-      <h2>{user.email}</h2>
-      <h2>User has redeemed a {voucher.voucher_type.name} voucher</h2>
+      <ul className="text-center">
+        <li style={{ textTransform: "none" }}>Email: {user.email}</li>
+        <li style={{ textTransform: "none" }}>ID: {user.id}</li>
+      </ul>
+      <h2 className="text-center">This user has redeemed a {voucher.voucher_type.name} voucher</h2>
       <div className={"flex gap-5 flex-col items-center md:flex-row"}>
-        <RedeemVoucher 
-        redeemed_user_id={userId} 
-        voucher_id={voucherId} 
-        uses={voucher.used ? voucher.used : null} 
-        max_uses={voucher.voucher_type ? voucher.voucher_type.uses_per_voucher : null}
-        voucher_user_id={voucher.user_id ? voucher.user_id : null}
-        user_email={user ? user.email : null}
-        times_used={voucher.used}
+        <RedeemVoucher
+          redeemed_user_id={userId}
+          voucher_id={voucherId}
+          uses={voucher.used ? voucher.used : null}
+          max_uses={voucher.voucher_type ? voucher.voucher_type.uses_per_voucher : null}
+          voucher_user_id={voucher.user_id ? voucher.user_id : null}
+          user_email={user ? user.email : null}
+          times_used={voucher.used}
         />
       </div>
     </div>
